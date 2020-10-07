498171b4e8978b3a8feb3a5279ab3efe2b193950
Matheus Henrique <matheusprogbr@gmail.com>
2020-10-06 22:57:55 -0300

    feat: Add ejs template engine and axios to send a GET request to the RESTful API

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
+const ejs = require("ejs");
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
 
+app.use(express.static("/public"));
 app.use(routes);
 
-
-
 app.listen(3000, () => {
-  console.log('Server is running!');
+  console.log("Server is running!");
 });
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,7 +1,14 @@
-const index = (req, res) => {
-  return res.send("Hello World! Here are the products!");
-};
+const axios = require("axios");
 
+const index = async (req, res) => {
+  try {
+    const result = await axios.get("http://localhost:3500/api/v1/products");
+    const products = result.data.data;
+    return res.render("products", { products });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   index,
