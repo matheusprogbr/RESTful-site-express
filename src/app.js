c8a28c1467a8117a73ba7f52de39dfab6eecebc5
Matheus Henrique <matheusprogbr@gmail.com>
2020-10-07 01:48:23 -0300

    style: Add a style file for the project

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
 
-app.use(express.static("/public"));
+app.use(express.static("public"));
 app.use(routes);
 
 app.listen(3000, () => {
